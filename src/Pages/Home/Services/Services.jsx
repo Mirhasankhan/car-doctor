49cd1b1bb99e74c51a354af6e2b2c596373552d7
Mirhasankhan <mirhasan000034@gmail.com>
2023-05-12 20:02:48 +0600

    car doctor started

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([])
+
+    useEffect(()=>{
+        fetch('service.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+    return (
+        <div>
+            <div className="text-2xl text-center space-y-4">
+                <h1 className=' text-orange-400'>Our Services</h1>
+                <h3 className='text-3xl font-bold'>Our Service Area</h3>
+                <p className='text-sm'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                    {
+                        services.map(service => <ServiceCard
+                            key={service._id}
+                            service={service}
+                        ></ServiceCard>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
